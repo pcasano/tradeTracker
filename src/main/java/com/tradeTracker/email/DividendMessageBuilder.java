38d4ff8d22528d043e5afd9ef2a11a5dc18ff93b
pcasano <pabloes87@gmail.com>
2021-12-26 23:15:40 +0100

    table Builder extended

--- src/main/java/com/tradeTracker/email/DividendMessageBuilder.java
+++ src/main/java/com/tradeTracker/email/DividendMessageBuilder.java
@@ -3,6 +3,9 @@ package com.tradeTracker.email;
 import com.tradeTracker.configuration.Configuration;
 import com.tradeTracker.reportContents.Company;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class DividendMessageBuilder extends MessageBuilder{
@@ -15,6 +18,44 @@ public class DividendMessageBuilder extends MessageBuilder{
     }
 
     public void sendDividendEmail() {
-        this.sendEmail();
+        this.sendEmail(
+                this.getHtmlTable(),
+                "Dividend on " + new SimpleDateFormat("dd/MM/yyyy").format(new Date(System.currentTimeMillis()-24*60*60*1000))
+        );
+    }
+
+    private String getHtmlTable() {
+        String htmlTableHeader = """
+                <table style="border-collapse: collapse; width: 100%; height: 72px;" border="1">
+                <tbody>
+                <tr style="height: 18px;">
+                <td style="width: 20%; height: 18px;"><span style="background-color: #ffffff;"><strong>Date</strong></span></td>
+                <td style="width: 20%; height: 18px;"><span style="background-color: #ffffff;"><strong>Company</strong></span></td>
+                <td style="width: 20%; height: 18px;"><span style="background-color: #ffffff;"><strong>Amount</strong></span></td>
+                <td style="width: 20%; height: 18px;"><span style="background-color: #ffffff;"><strong>Taxes</strong></span></td>
+                <td style="width: 20%; height: 18px;"><span style="background-color: #ffffff;"><strong>Net</strong></span></td>
+                <td style="width: 20%; height: 18px;"><span style="background-color: #ffffff;"><strong>Currency</strong></span></td>
+                </tr>
+                """;
+        StringBuilder sb = new StringBuilder(htmlTableHeader);
+        listOfCompanies.forEach(company -> sb.append(getTableRow(company)));
+
+        sb.append("""
+                </tr>
+                </tbody>
+                </table>""");
+        return sb.toString();
+    }
+
+    private String getTableRow(Company company) {
+        DecimalFormat df = new DecimalFormat("0.00");
+        return  "<tr style=\"height: 18px;\">\n" +
+                "<td style=\"width: 20%; height: 18px;\">"+new SimpleDateFormat("dd/MM/yyyy").format(company.getPaymentDate()) +"</td>\n" +
+                "<td style=\"width: 20%; height: 18px;\">"+company.getCompanyName()+"</td>\n" +
+                "<td style=\"width: 20%; height: 18px;\">"+company.getAmount()+"</td>\n" +
+                "<td style=\"width: 20%; height: 18px;\">"+company.getTax()+ " (" + df.format(-100*company.getTax()/company.getAmount())+"%)</td>\n" +
+                "<td style=\"width: 20%; height: 18px;\">"+df.format(Double.valueOf(company.getAmount() + company.getTax()))+"</td>\n" +
+                "<td style=\"width: 20%; height: 18px;\">"+company.getCurrency()+"</td>\n" +
+                "</tr>\n";
     }
 }
--- src/main/java/com/tradeTracker/email/MessageBuilder.java
+++ src/main/java/com/tradeTracker/email/MessageBuilder.java
@@ -18,7 +18,7 @@ public class MessageBuilder {
         this.configuration = configuration;
     }
 
-    protected void sendEmail() {
+    protected void sendEmail(String table, String subject) {
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
@@ -31,10 +31,10 @@ public class MessageBuilder {
             msg.setFrom(configuration.getEmailData().getEmailSender());
             msg.setRecipients(Message.RecipientType.TO,
                     configuration.getEmailData().getTargetEmail());
-            msg.setSubject("JavaMail hello world example");
+            msg.setSubject(subject);
             msg.setSentDate(new Date());
             msg.setText("Hello, world!\n");
-            msg.setContent("<html><body><table><tr><td>Bubu<td>Lala</tr></table></body></html>", "text/html");
+            msg.setContent(table, "text/html");
             Transport.send(msg, configuration.getEmailData().getEmailSender(), configuration.getEmailData().getEmailSenderPass());
         } catch (MessagingException mex) {
             System.out.println("send failed, exception: " + mex);
